86148ae25859cde9bfd231e928c4da4089630dfe
ChristianBuan <77389628+ChristianBuan@users.noreply.github.com>
2021-01-19 14:21:58 +0800

    Update header.php

--- header.php
+++ header.php
@@ -6,6 +6,7 @@ session_start();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="icon.png" />
     <title>NovGenT Dictionary</title>
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Barlow:wght@500&family=IBM+Plex+Sans&display=swap" rel="stylesheet">
