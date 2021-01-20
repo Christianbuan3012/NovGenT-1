301291615df7b8c4559218b0ebefb5aa70664d6f
ChristianBuan <77389628+ChristianBuan@users.noreply.github.com>
2021-01-20 21:50:43 +0800

    Update setUp.php

--- include/setUp.php
+++ include/setUp.php
@@ -1,8 +1,8 @@
 <?php
-$server = 'localhost';
-$user = 'root';
-$password = '';
-$database = 'NovGent';
+$server = 'remotemysql.com';
+$user = 'BtwLmu6tTk';
+$password = 'QPxRh8dwnl';
+$database = 'BtwLmu6tTk';
 
 // Create connection to localhost
 $connect = new mysqli($server, $user, $password);
@@ -25,6 +25,7 @@ if($connect->select_db($database) === false) {
 function makeTables($connect) {
     $usertable = "CREATE TABLE IF NOT EXISTS users (
         userId INT NOT NULL AUTO_INCREMENT,
+        pin VARCHAR(255) NOT NULL,
         username VARCHAR(255) NOT NULL,
         password VARCHAR(255) NOT NULL,
         type VARCHAR(255) NOT NULL DEFAULT 'Author',
@@ -34,27 +35,30 @@ function makeTables($connect) {
 
     //Create the one and only admin with hashed password, new users will by default be authors
     $adminPwd = password_hash('Admin123', PASSWORD_DEFAULT);
-    $newAdmin = "INSERT INTO users(username, password, type)
-                VALUES ('Admin','$adminPwd','Admin')";
+    $newAdmin = "INSERT INTO users(pin, username, password, type)
+                VALUES ('2021', 'Admin','$adminPwd','Admin')";
     $connect->query($newAdmin);
 
     //Create topics table
-    $topicTable = "CREATE TABLE IF NOT EXISTS topics (
+    $topicTable = "CREATE TABLE IF NOT EXISTS topic (
         topicId INT NOT NULL AUTO_INCREMENT,
         topicTitle VARCHAR (255) NOT NULL,
         createdBy INT NOT NULL,
-        CONSTRAINT primaryKey_topics PRIMARY KEY (topicId),
+        CONSTRAINT primaryKey_topic PRIMARY KEY (topicId),
         CONSTRAINT foreignKey_user FOREIGN KEY (createdBy) REFERENCES users(userId) ON UPDATE CASCADE ON DELETE CASCADE
         );";
     $connect->query($topicTable);
-
     //Insert dummy topic data only for viewing purposes, created by "admin"
-    $topicEntry = "INSERT INTO topics(topicTitle, createdBy) VALUES ('Horses', 1)";
+    $topicEntry = "INSERT INTO topic(topicTitle, createdBy) VALUES ('English', 1)";
+    $connect->query($topicEntry);
+    $topicEntry = "INSERT INTO topic(topicTitle, createdBy) VALUES ('Filipino', 1)";
+    $connect->query($topicEntry);
+    $topicEntry = "INSERT INTO topic(topicTitle, createdBy) VALUES ('Spanish', 1)";
     $connect->query($topicEntry);
 
     //Add index to the topic table, as this will be frequently searched
-    $indexTopic = "ALTER TABLE topics ADD FULLTEXT (topicTitle);";
-    $connect->query($indexTopic);
+    $indextopic = "ALTER TABLE topic ADD FULLTEXT (topicTitle);";
+    $connect->query($indextopic);
 
 
 
@@ -63,30 +67,31 @@ function makeTables($connect) {
         entryId INT NOT NULL AUTO_INCREMENT,
         entryTitle VARCHAR(255) NOT NULL,
         description VARCHAR(1000) NOT NULL,
+        entryexample VARCHAR(255) NOT NULL,
         dateCreated DATETIME DEFAULT NOW() NOT NULL,
         createdBy INT NOT NULL,
         topicId INT NOT NULL,
         CONSTRAINT primaryKey_entries PRIMARY KEY (entryId),
         CONSTRAINT foreignKey_users FOREIGN KEY (createdBy) REFERENCES users(userId) ON UPDATE CASCADE ON DELETE CASCADE,
-        CONSTRAINT foreignKey_topics FOREIGN KEY (topicId) REFERENCES topics(topicId) ON UPDATE CASCADE ON DELETE CASCADE
+        CONSTRAINT foreignKey_topics FOREIGN KEY (topicId) REFERENCES topic(topicId) ON UPDATE CASCADE ON DELETE CASCADE
         );";
     $connect->query($entryTable);
 
     //Insert dummy entry data only for viewing purposes, created by "admin"
     $date = date('Y-m-d H:i:s');
-    $entryEntry = "INSERT INTO entries(entryTitle, description, dateCreated, createdBy, topicId) 
-                    VALUES ('Brown horses','Brown horses with white dots are the best', '$date', 1, 1)";
+    $entryEntry = "INSERT INTO entries(entryTitle, description, entryexample, dateCreated, createdBy, topicId) 
+                    VALUES ('Brown horses','Brown horses with white dots are the best', 'A big brown horse', '$date', 1, 1)";
     $connect->query($entryEntry);
 
     //Add index to the entry table, as this will be frequently searched
-    $indexEntry = "ALTER TABLE entries ADD FULLTEXT (entryTitle, description);";
+    $indexEntry = "ALTER TABLE entries ADD FULLTEXT (entryTitle, description, entryexample);";
     $connect->query($indexEntry);
 
     $Translation = "CREATE TABLE IF NOT EXISTS Translations(
-        languageId INT NOT NULL AUTO_INCREMENT,
+        topicId INT NOT NULL AUTO_INCREMENT,
         translations VARCHAR (255) NOT NULL,
         createdBy INT NOT NULL,
-        CONSTRAINT primaryKey_language PRIMARY KEY (languageId),
+        CONSTRAINT primaryKey_topic PRIMARY KEY (topicId),
         CONSTRAINT foreignKey_userss FOREIGN KEY (createdBy) REFERENCES users(userId) ON UPDATE CASCADE ON DELETE CASCADE
         );";
     $connect->query($Translation);
