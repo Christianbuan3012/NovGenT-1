69e1eba157dc5d014753d3226e1c19866604c2ce
ChristianBuan <77389628+ChristianBuan@users.noreply.github.com>
2021-01-20 21:51:17 +0800

    Update and rename topicsMenu.php to topicMenu.php

--- include/topicMenu.php
+++ include/topicMenu.php
@@ -4,11 +4,11 @@ include 'connect.php';
 /* This is very little code that only displays the titles from topics in
     a select dropdown for the users, when they make a new entry in create.php.*/
 
-$result = $connection->query("SELECT topicId, topicTitle FROM topics");
+$result = $connection->query("SELECT topicId, topicTitle FROM topic");
 $checkResult = mysqli_num_rows($result);
 
 if($checkResult < 0) { 
-    echo '<p>There are no available topics. Please create one first.</p>';
+    echo '<p>There are no available languages. Please create one first.</p>';
 } else {
     while ($list = $result->fetch_assoc()) {
         $topicId = $list['topicId'];
