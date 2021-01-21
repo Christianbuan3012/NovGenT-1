031838de30dc0b84d5bd1c3b7c28a9ab4fc8ee6b
ChristianBuan <77389628+ChristianBuan@users.noreply.github.com>
2021-01-21 13:15:27 +0800

    Update connect.php

--- include/connect.php
+++ include/connect.php
@@ -2,9 +2,9 @@
 
 //Define database variables
 define('DB_SERVER', 'remotemysql.com');
-define('DB_USERNAME', 'YGjvSRRMm7');
-define('DB_PASSWORD', 'VLK9ZfZtYJ');
-define('DB_NAME', 'YGjvSRRMm7');
+define('DB_USERNAME', 'VzW7WZ8LJO');
+define('DB_PASSWORD', 'XjJpXHTDZA');
+define('DB_NAME', 'VzW7WZ8LJO');
 
 // Create connection to localhost
 $connection = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
