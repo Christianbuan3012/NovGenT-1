59a9375767fb9171fc44d706679ab0f9ab13fb85
ChristianBuan <77389628+ChristianBuan@users.noreply.github.com>
2021-01-22 17:27:55 +0800

    Update header.php

--- header.php
+++ header.php
@@ -43,7 +43,7 @@ session_start();
                 <?php endif ?>
             </ul>
 
-            <form action="search.php" method="POST" style="padding-top: 20px;">
+            <form action="search.php" method="POST">
                 <input type="search" name="search" id="search" placeholder="Search..." style="width: 400px;">
                 <input type="submit" name="searchbutton" value="Search" style="width: 50px; padding-right: 100px;">
             </form>
