8c7d3a731d6bdae714e1297a28efc25b14906425
ChristianBuan <77389628+ChristianBuan@users.noreply.github.com>
2021-01-22 14:41:22 +0800

    Update topicMenu.php

--- include/topicMenu.php
+++ include/topicMenu.php
@@ -4,7 +4,7 @@ include 'connect.php';
 /* This is very little code that only displays the titles from topics in
     a select dropdown for the users, when they make a new entry in create.php.*/
 
-$result = $connection->query("SELECT topicId, topicTitle FROM topic");
+$result = $connection->query("SELECT topicId, topicTitle FROM topics");
 $checkResult = mysqli_num_rows($result);
 
 if($checkResult < 0) { 
