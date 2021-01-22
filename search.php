cb27dda891795baaeda82bc502e57e8378de39b8
ChristianBuan <77389628+ChristianBuan@users.noreply.github.com>
2021-01-22 15:43:42 +0800

    Update search.php

--- search.php
+++ search.php
@@ -9,7 +9,7 @@
                 //Check that the user did not write code etc in the search field, and get the input
                 $searchInput = mysqli_real_escape_string($connection, $_POST['search']); 
                 $sql = "SELECT t.*, e.* FROM topics t LEFT OUTER JOIN entries e ON t.topicId = e.topicId 
-                WHERE MATCH(t.topicTitle) AGAINST('*$searchInput*' IN BOOLEAN MODE) OR
+                WHERE MATCH(t.topicTitle) AGAINST('*$searchInput*' IN BOOLEAN MODE) AND
                 MATCH(e.entryTitle, e.description) AGAINST('*$searchInput*' IN BOOLEAN MODE);"; 
                 $searchResult = mysqli_query($connection, $sql); 
                 $finalResult = mysqli_num_rows($searchResult); //Checking if we have any result from $searchResult
@@ -21,7 +21,7 @@
                         '<p>' . $list["description"] . '</p><hr>';
                     }
                 } else { //If there are 0 results
-                    echo '<p>There are ' . $finalResult . ' results your search on: "' . $searchInput . '"!</p>';
+                    echo '<p>There are 0 results your search on: "' . $searchInput . '"!</p>';
                 }
             }
         ?>
