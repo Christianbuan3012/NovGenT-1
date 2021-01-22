7dfb2bb048b770ff5467ffc93783f7468b5fa470
ChristianBuan <77389628+ChristianBuan@users.noreply.github.com>
2021-01-22 17:18:42 +0800

    Update search.php

--- search.php
+++ search.php
@@ -5,26 +5,24 @@
     <div class="mainbox">
         <h2>Search results</h2>
         <?php 
-            if(isset($_POST['searchbutton'])) {
-                //Check that the user did not write code etc in the search field, and get the input
-                $searchInput = mysqli_real_escape_string($connection, $_POST['search']); 
-                $sql = "SELECT t.*, e.* FROM topics t LEFT OUTER JOIN entries e ON t.topicId = e.topicId 
-                WHERE MATCH(t.topicTitle) AGAINST('*$searchInput*' IN BOOLEAN MODE) OR
-                MATCH(e.entryTitle, e.description) AGAINST('*$searchInput*' IN BOOLEAN MODE);"; 
-                $searchResult = mysqli_query($connection, $sql); 
-                $finalResult = mysqli_num_rows($searchResult); //Checking if we have any result from $searchResult
-                if($finalResult > 0) { //If there are more than 0 results
-                    echo '<p>There are ' . $finalResult . ' results from your search on "' . $searchInput . '":</p>';
-                    while($list = mysqli_fetch_assoc($searchResult)) { //Display the results
-                        echo '<h3> Language: ' . $list["topicTitle"] . '</h3>' . 
-                        '<h4>' . $list["entryTitle"] . '</h4>' . 
-                        '<p>' . $list["description"] . '</p><hr>';
-                    }
-                } else { //If there are 0 results
-                    echo '<p>There are 0 results your search on: "' . $searchInput . '"!</p>';
-                }
-            }
-        ?>
+$search = $_GET['search'];
+$con = mysqli_connect("remotemysql.com", "VzW7WZ8LJO", "XjJpXHTDZA", "VzW7WZ8LJO");
+    $sql = "SELECT * FROM entries WHERE MATCH(entryTitle, description) AGAINST ('%" .$search. "%')";
+    $run = mysqli_query($con, $sql);
+    $foundnum = mysqli_num_rows($run);
+    if ($foundnum == 0){
+        echo "There are 0 results from your search on ' . $search . ':";
+    } else {
+        echo "There are $foundnum results from your search on ' . $search . ':";
+        $sql  = "SELECT * FROM entries WHERE MATCH(entryTitle, description) AGAINST ('%" .$search. "%')";
+        $getquery = mysqli_query($con, $sql);
+        while ($runrows = mysqli_fetch_array($getquery)) {
+            echo '<h3> Language: ' . $list["topicTitle"] . '</h3>' . 
+                 '<h4>' . $list["entryTitle"] . '</h4>' . 
+                 '<p>' . $list["description"] . '</p><hr>';
+        }
+    }
+?>
     </div> 
 </body>
 </html>
