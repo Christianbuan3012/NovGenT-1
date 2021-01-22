c9aad1c9ac628e568aff74790d11ce5c978fc129
ChristianBuan <77389628+ChristianBuan@users.noreply.github.com>
2021-01-22 16:49:09 +0800

    Update search.php

--- search.php
+++ search.php
@@ -1,71 +1,40 @@
 <?php 
-session_start();
+    require 'header.php';
+    include 'include/connect.php'; 
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>NovGenT Online Dictionary</title>
-    <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Barlow:wght@500&family=IBM+Plex+Sans&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="styles.css">
-    <script type="text/javascript">
-    window.addEventListener("load", function () {
-    const loader = document.querySelector(".loader");
-    loader.className += " hidden";
-    });
-</script>
-</head>
-<body>
-    <div class="loader">
-        <img src="loading.gif" alt="Loading..." />
-    </div>
-    <header>
-        <nav>
+    <div class="mainbox">
+        <h2>Search results</h2>
+        <?php 
+            $button = $_GET['searchbutton'];
+            $search = $_GET['search'];
+            $con = mysqli_connect("remotemysql.com", "VzW7WZ8LJO", "XjJpXHTDZA", "VzW7WZ8LJO");
+                $sql = "SELECT * FROM entries WHERE MATCH(entryTitle, description) AGAINST ('%" .$search. "%')";
+                $run = mysqli_query($connection, $sql);
+                $foundnum = mysqli_num_rows($run);
+                if ($foundnum == 0){
+                    echo "There are 0 results from your search on ' . $search . ':";
+                } else {
+                    echo "There are $foundnum results from your search on \"' . $search . '\":";
+                    $sql  = "SELECT * FROM entries WHERE MATCH(entryTitle, description) AGAINST ('%" .$search. "%')";
+                    $getquery = mysqli_query($connection, $sql);
+                    while ($runrows = mysqli_fetch_array($getquery)) {
+                        echo '<h3> Language: ' . $list["topicTitle"] . '</h3>' . 
+                             '<h4>' . $list["entryTitle"] . '</h4>' . 
+                             '<p>' . $list["description"] . '</p><hr>';
+                    }
+                }
+        ?>
+    </div> 
+<footer>
+    <nav>
             <ul>
-                <li><a style="font-size: 25px; font-weight: 900;" href="index.php">NovGenT</a></li>
-                <li><a href="index.php">Home</a></li>
-                <?php if(!isset($_SESSION['username'])) : ?>
-                    <li><a href="newAccount.php">Sign up</a></li>
-                <?php endif ?>
-
-                <?php if(isset($_SESSION['username'])) : ?>                                      
-                    <li><a href="userProfile.php">Account Settings</a></li>
-                    <li><a href="create.php">Create an Entry</a></li>
-                <?php endif ?>
-
-                <?php if(isset($_SESSION['usertype'])) : ?>
-                    <?php if($_SESSION['usertype'] == 'Admin') : ?>
-                    <li><a href="userList.php">Manage Users</a></li>
-                    <li><a href="entryList.php">Manage Entries</a></li>                  
-                    <?php endif ?>
-                <?php endif ?>
+                <li><a style="font-size: 22px;" href="About.php">About NovGenT</a></li>
+                <li><a style="font-size: 22px;" href="guidelines.php">NovGenT's Guidelines</a></li>
+                <li><a href="www.facebook.com" style="font-weight: 700; font-size: 20px;">Facebook</a></li>
+                <li><a href="www.twitter.com"  style="font-weight: 700; font-size: 20px;">Twitter</a></li>
             </ul>
 
-            <form action="search.php" method="POST" style="padding-top: 20px;">
-                <input type="search" name="search" id="search" placeholder="Search..." style="width: 400px;">
-                <input type="submit" name="searchbutton" value="Search" style="width: 50px; padding-right: 100px;">
-            </form>
-
-            <div class="signInOut">
-                <?php 
-                if(isset($_SESSION['username'])) {
-                    $username = $_SESSION['username'];
-                    echo "
-                    <form action='include/dbHandler.php' method='post'>
-                    <input type='submit' name='logout' id='logout' value='Log out'>
-                    </form>";
-                } else {
-                    echo '<form action="include/dbHandler.php" method="post">
-                    <label for="username">Username:</label>
-                    <input type="text" name="username" id="username" required="">
-                    <label for="password">Password:</label>
-                    <input type="password" name="password" id="password" required="">
-                    <input type="submit" name="login" id="submit" value="Log in">
-                    </form>';
-                }
-                ?>
-            </div>
-        </nav>
-    </header>
+    </nav>
+</footer>
+</body>
+</html>
