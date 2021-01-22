c89ff15868c4926b35cfb8133a43a88e13b66b7b
ChristianBuan <77389628+ChristianBuan@users.noreply.github.com>
2021-01-22 16:51:23 +0800

    Update search.php

--- search.php
+++ search.php
@@ -5,25 +5,26 @@
     <div class="mainbox">
         <h2>Search results</h2>
         <?php 
-            $button = $_GET['searchbutton'];
-            $search = $_GET['search'];
-            $con = mysqli_connect("remotemysql.com", "VzW7WZ8LJO", "XjJpXHTDZA", "VzW7WZ8LJO");
-                $sql = "SELECT * FROM entries WHERE MATCH(entryTitle, description) AGAINST ('%" .$search. "%')";
-                $run = mysqli_query($connection, $sql);
-                $foundnum = mysqli_num_rows($run);
-                if ($foundnum == 0){
-                    echo "There are 0 results from your search on ' . $search . ':";
-                } else {
-                    echo "There are $foundnum results from your search on \"' . $search . '\":";
-                    $sql  = "SELECT * FROM entries WHERE MATCH(entryTitle, description) AGAINST ('%" .$search. "%')";
-                    $getquery = mysqli_query($connection, $sql);
-                    while ($runrows = mysqli_fetch_array($getquery)) {
-                        echo '<h3> Language: ' . $list["topicTitle"] . '</h3>' . 
-                             '<h4>' . $list["entryTitle"] . '</h4>' . 
-                             '<p>' . $list["description"] . '</p><hr>';
-                    }
-                }
-        ?>
+
+$button = $_GET['searchbutton'];
+$search = $_GET['search'];
+$con = mysqli_connect("remotemysql.com", "VzW7WZ8LJO", "XjJpXHTDZA", "VzW7WZ8LJO");
+    $sql = "SELECT * FROM entries WHERE MATCH(entryTitle, description) AGAINST ('%" .$search. "%')";
+    $run = mysqli_query($con, $sql);
+    $foundnum = mysqli_num_rows($run);
+    if ($foundnum == 0){
+        echo "There are 0 results from your search on ' . $search . ':";
+    } else {
+        echo "There are $foundnum results from your search on \"' . $search . '\":";
+        $sql  = "SELECT * FROM entries WHERE MATCH(entryTitle, description) AGAINST ('%" .$search. "%')";
+        $getquery = mysqli_query($con, $sql);
+        while ($runrows = mysqli_fetch_array($getquery)) {
+            echo '<h3> Language: ' . $list["topicTitle"] . '</h3>' . 
+                 '<h4>' . $list["entryTitle"] . '</h4>' . 
+                 '<p>' . $list["description"] . '</p><hr>';
+        }
+    }
+?>
     </div> 
 <footer>
     <nav>
