83726065bbe8573b1d501691fb1851dbf651af21
ChristianBuan <77389628+ChristianBuan@users.noreply.github.com>
2021-01-23 11:55:39 +0800

    Update index.php

--- index.php
+++ index.php
@@ -36,7 +36,7 @@
         <?php if(!isset($_SESSION['username'])) : ?>
         Log in, or sign up now to create your own!</p>
         <?php endif ?>
-        <hr>
+        <hr style="height: 5px;">
     </div>
 
     <!-----------------Left column displaying all topics:------------------------->
