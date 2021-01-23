acb5b42b02180d876464721db838719af7330c0c
ChristianBuan <77389628+ChristianBuan@users.noreply.github.com>
2021-01-23 12:03:35 +0800

    Update index.php

--- index.php
+++ index.php
@@ -63,7 +63,7 @@
             <!--Form for sorting the topics-->
             <form action="index.php" method="get">
             <select name="sortingOption" id="sortingOption" style="font-size: 14px;border-width: 2px; padding: 9px;background:white;border-style: solid;border-color: black;color: #1DA1F2;font-weight: 900;font-family: 'IBM Plex Sans';border-radius: 5px;">
-            <option value="chronological" <?php if($sort == "Alphabetical"){echo "selected";}?>>By Alphabetical</option>
+            <option value="Alphabetical" <?php if($sort == "Alphabetical"){echo "selected";}?>>By Alphabetical</option>
             <option value="popularity" <?php if($sort == "popularity"){echo "selected";}?>>By popularity</option></select> 
             <input type="submit" name="sorttopic" value="Sort language" id="kwan">
             </form>
