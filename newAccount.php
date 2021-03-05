290110bf3da7659849f6a809f8d7c2f5f01d1ee0
ChristianBuan <77389628+ChristianBuan@users.noreply.github.com>
2021-03-06 06:34:02 +0800

    Update newAccount.php

--- newAccount.php
+++ newAccount.php
@@ -9,14 +9,14 @@ include 'header.php'; ?>
         <form action="include/dbHandler.php" method="post" style="font-weight: 700;">
             <label for="username">Username:</label><br>
             <input type="text" name="username" id="username"><br><br>
-            <label for="pin">PIN (For Password Recovery):</label><br>
+            <label for="pin">PIN:</label><br>
             <input type="password" name="pin" id="pin"><br><br>
             <label for="password">Password:</label><br>
             <input type="password" name="password" id="password"><br><br>
             <label for="password2">Confirm Password:</label><br>
             <input type="password" name="confPassword" id="confPassword"><br>
             <p><a href="guidelines.php" style="text-decoration: none; color: #1DA1F2;">NovGenT's Guidelines</p>
-            <input type="submit" name="submit" id="submit" value="Create account">
+            <input type="submit" name="submit" id="submit" value="Create account" style="background-color:#03C04A; color: white; ">
         </form>
 
         <?php //Display error messages, values passed from register in dbHandler.php
@@ -24,30 +24,19 @@ include 'header.php'; ?>
                 if($_GET["error"] == "passwordnotmatch") {
                     echo '<p class="errorMess">Passwords do not match</p>';
                 } elseif($_GET["error"] == "useralreadytaken") {
-                    echo '<p class="errorMess">Username already exist. Please choose another one.</p>';
+                    echo '<p class="errorMess">Username already exist.</p>';
                 } elseif($_GET["error"] == "emptyfields") {
                     echo '<p class="errorMess">Please fill in all the fields.</p>';
                 } elseif($_GET["error"] == "invalidusername") {
                     echo '<p class="errorMess">Your username can not contain special characters. Only use letters and numbers.</p>';
                 } elseif($_GET["error"] == "invalidpin") {
-                echo '<p class="errorMess">PIN should be minimum 5 characters and max 16 characters.</p>';
+                echo '<p class="errorMess">PIN should be minimum 3 characters and max 5 characters.</p>';
                 } elseif($_GET["error"] == "invalidpassword") {
-                echo '<p class="errorMess">Password should be minimum 8 characters and max 16 characters.</p>';
+                echo '<p class="errorMess">Password should be minimum 5 characters and max 16 characters.</p>';
                 }
             }
         ?>
 
     </div>
-<footer style="padding-top: 400px;">
-    <nav>
-            <ul>
-                <li><a style="font-size: 22px;" href="About.php">About NovGenT</a></li>
-                <li><a style="font-size: 22px;" href="guidelines.php">NovGenT's Guidelines</a></li>
-                <li><a href="https://www.facebook.com/kitsune.christian.12/" style="font-weight: 700; font-size: 20px;">Facebook</a></li>
-                <li><a href="www.twitter.com"  style="font-weight: 700; font-size: 20px;">Twitter</a></li>
-            </ul>
-
-    </nav>
-</footer>
 </body>
 </html>
