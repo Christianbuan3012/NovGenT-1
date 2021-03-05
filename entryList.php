0c239008a27eb5c18625acac34387d3059f7a91a
ChristianBuan <77389628+ChristianBuan@users.noreply.github.com>
2021-03-06 06:33:01 +0800

    Update entryList.php

--- entryList.php
+++ entryList.php
@@ -39,17 +39,5 @@
             ?>
         </table>
 </div>
-<footer style="padding-top: 400px;">
-    <nav>
-            <ul>
-                <li style="text-align: center; padding-right: 20px;">©2020-2021 NovGenT Dictionary </li>
-                <li><a style="font-size: 22px;" href="About.php">About NovGenT</a></li>
-                <li><a style="font-size: 22px;" href="guidelines.php">NovGenT's Guidelines</a></li>
-                <li><a href="https://www.facebook.com/kitsune.christian.12/" style="font-weight: 700; font-size: 20px;">Facebook</a></li>
-                <li><a href="www.twitter.com"  style="font-weight: 700; font-size: 20px; padding-right: 30px;">Twitter</a></li>
-            </ul>
-
-    </nav>
-</footer>
 </body>
 </html> 
