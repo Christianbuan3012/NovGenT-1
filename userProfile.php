4f9fc12deb120788a68b71ca5ddaa81ff2a60f63
ChristianBuan <77389628+ChristianBuan@users.noreply.github.com>
2021-03-06 06:35:34 +0800

    Update userProfile.php

--- userProfile.php
+++ userProfile.php
@@ -36,6 +36,8 @@
                 </form>
             <?php endif ?>
             </div>
+            <br>
+            <br>
 
             <!--------------------Right column where user can update password:--------------->
             <div>
@@ -75,16 +77,5 @@
             </div>
         </div>
     </div>
-<footer style="padding-top: 400px;">
-    <nav>
-            <ul>
-                <li><a style="font-size: 22px;" href="About.php">About NovGenT</a></li>
-                <li><a style="font-size: 22px;" href="guidelines.php">NovGenT's Guidelines</a></li>
-                <li><a href="https://www.facebook.com/kitsune.christian.12/" style="font-weight: 700; font-size: 20px;">Facebook</a></li>
-                <li><a href="www.twitter.com"  style="font-weight: 700; font-size: 20px; padding-right: 30px;">Twitter</a></li>
-            </ul>
-
-    </nav>
-</footer>
 </body>
 </html>  
